d3f61b5fdde488e2094ecc09504a851faa3f9d87
groovypark <soobin950@gmail.com>
2018-08-11 14:12:13 +0900

    Update: weather

--- weather/src/components/Weather.vue
+++ weather/src/components/Weather.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-container fluid class="text-white">
+  <div fluid class="text-white">
     <v-slide-y-transition mode="out-in">
-      <v-layout v-layout justify-center>
+      <v-layout justify-center column>
         <div class="weather-icon">
           <v-icon v-if="weather === 'good'">
             sentiment_very_satisfied
@@ -13,9 +13,22 @@
             sentiment_very_dissatisfied
           </v-icon>
         </div>
+        <div class="temperature">
+          87
+        </div>
+        <p>
+          강남구의 대기는 지금 보통!
+        </p>
+        <v-layout align-center justify-center row fill-height>
+          <div class="dust">
+            <div>미세먼지</div>
+            <div>오존</div>
+            <div>초미세먼지</div>
+          </div>
+        </v-layout>
       </v-layout>
     </v-slide-y-transition>
-  </v-container>
+  </div>
 </template>
 
 <script>
@@ -32,5 +45,26 @@ export default {
   /* height: 50px; */
   color: white;
   font-size: 90px;
+  padding-top: 25%;
+}
+.temperature {
+  font-size: 50px !important;
+  font-weight: lighter;
+}
+p {
+  font-weight: bold;
+  font-size: 20px;
+}
+li {
+  list-style: none;
+  width:10em;
+  float: left;
+}
+.dust {
+  background-color: rgba(255, 255, 255, 0.5);
+  width: 100%;
+  height: 30%;
+  position: absolute;
+  bottom: 0;
 }
 </style>
