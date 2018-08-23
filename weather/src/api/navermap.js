5ceb39102fd1adcce5f5ab39597f35347ae9a6cf
groovypark <soobin950@gmail.com>
2018-08-23 20:26:51 +0900

    Delete reject

--- weather/src/api/navermap.js
+++ weather/src/api/navermap.js
@@ -1,7 +1,7 @@
 const naver = window.naver;
 
 export function getAddressFromGeocode(x, y) {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     naver.maps.Service.reverseGeocode(
       {
         location: new naver.maps.LatLng(x, y),
